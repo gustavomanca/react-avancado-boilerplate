d95cb8c6ad72eff872c9da84803147d13257279c
Gustavo Manca <gustavo.manca@pixter.com.br>
2021-05-27 00:51:58 -0300

    feat: create test to check background color

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -6,9 +6,16 @@ describe('<Main />', () => {
   it('should render the heading', () => {
     const { container } = render(<Main />)
 
-    expect(screen.getByRole('heading', { name: /react avançado/i }))
-      .toBeInTheDocument
+    expect(
+      screen.getByRole('heading', { name: /advanced react/i })
+    ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render colors correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
