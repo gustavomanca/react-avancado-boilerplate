2a66173e08ebe903f2e3f29a513c7531dd508e75
Gustavo Manca <gustavo.manca@pixter.com.br>
2021-05-27 00:51:14 -0300

    style: create component initial styles

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,18 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h2>React Avançado</h2>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/images/logo.svg"
+      alt="An atom image with Advanced React wrote next to"
+    />
+    <S.Title>Advanced React</S.Title>
+    <S.Description>TypeScript, NextJS and Styled Components</S.Description>
+    <S.Illustration
+      src="/images/hero-illustration.svg"
+      alt="A developer in front a screen displaying programming codes"
+    />
+  </S.Wrapper>
 )
 
 export default Main
