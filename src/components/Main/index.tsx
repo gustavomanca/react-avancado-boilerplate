babb45160c89c831a4c3725f4158b7c7bf24cc4e
Gustavo Manca <gustavo.manca@pixter.com.br>
2021-05-27 01:46:43 -0300

    feat: install and setup storybook
    - create first story

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,13 +1,16 @@
 import * as S from './styles'
 
-const Main = () => (
+const Main = ({
+  title = 'Advanced React',
+  description = 'TypeScript, NextJS and Styled Components'
+}) => (
   <S.Wrapper>
     <S.Logo
       src="/images/logo.svg"
       alt="An atom image with Advanced React wrote next to"
     />
-    <S.Title>Advanced React</S.Title>
-    <S.Description>TypeScript, NextJS and Styled Components</S.Description>
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
     <S.Illustration
       src="/images/hero-illustration.svg"
       alt="A developer in front a screen displaying programming codes"
